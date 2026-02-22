d1460fac5f8ccc97078fd6790271334ee7a91800
eliezerkenya <dev.eliezer.media@gmail.com>
2026-02-22 07:01:00 +0300

    version 1.1

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import logic.links as links
 from engine import app, db
 import utils.models as mods
+from a2wsgi import ASGIMiddleware as production
 
 with app.app_context():
     db.create_all()
 
-app.run(debug=True, host="0.0.0.0")
+app = production(app)                                 
